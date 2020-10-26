85ef0d7724119f55b4eeec4317c181e14f92314d
Diego Luces <55496726+DiegoLuces01@users.noreply.github.com>
2020-10-26 14:18:05 -0500

    Update llamada error.js

--- clase2 (API)/llamada error.js
+++ clase2 (API)/llamada error.js
@@ -4,14 +4,11 @@ axios
     "https://api.nasa.gov/planetary/apod?api_key=2uPghFtuIyGCNKsFk4fitHKbF73wwsh0818Xh646"
   )
   .then(function (response) {
-    // Esto se ejecuta al tener éxito con la solicitud
     console.log(response.data.url);
     img.src = response.data.url;
   })
   .catch(function (error) {
-    // Esto se ejecuta cuando hay un error con la solicitud
     console.log(error);
   })
   .then(function () {
-    // Esto se ejecuta siempre
   });
